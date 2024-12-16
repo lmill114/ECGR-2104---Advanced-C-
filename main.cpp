3effcacde9567bd9f4eb41b9c360f39052e469dd
lmill114 <lmill114@charlotte.edu>
2024-12-15 20:01:23 -0500

    Add files via upload
    With test text files.

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    
+    ifstream filestream;
+    filestream.open("test");
+    
+    ofstream oFile;
+    oFile.open("goodbye.txt");
+    
+    string text;
+    
+    if(!filestream.is_open()){
+        cout << "File is not open" << endl;
+    }
+    
+    if(filestream.bad()){
+        cout << "File is bad" << endl;
+    }
+    
+    if(filestream.good()){
+        cout << "File is good" << endl;
+    }
+    
+    while(!filestream.eof() && filestream.is_open()){
+        //filestream >> text;
+        getline(filestream,text);
+        oFile << text << "\n";
+    }
+    
+    filestream.close();
+    oFile.close();
+    
+    
+    return 0;
+}
