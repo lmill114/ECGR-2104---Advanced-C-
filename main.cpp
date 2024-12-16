b30230dd4659965108da73bcdb5b7f2e04354117
lmill114 <lmill114@charlotte.edu>
2024-12-15 19:35:33 -0500

    Add files via upload
    CH9 - Streams

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int x = 500;
+    double y = 10.36874578700469043239;
+    
+    
+    cin >> x;
+    
+    cout << "Hello World \n" << endl;
+    
+    cout << setprecision(5) << setprecision(2) << y << endl;
+    
+    return 0;
+}
